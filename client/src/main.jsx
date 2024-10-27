28ad715a408b431b4f6c741729f3dd77bcd94afd
kv422 <116920363+kv422@users.noreply.github.com>
2024-10-27 09:57:32 -0700

    Imported css file.

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 
+// Import our custom CSS
+import './scss/stylesheet.scss';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
